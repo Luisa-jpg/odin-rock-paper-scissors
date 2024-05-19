56deba97cc20fcc96ba3d896313a20ddd314e9a8
Luisa-jpg <luisa.metzen@arcor.de>
2024-05-19 22:09:43 +0200

    Clean code, add functions and message div

--- script.js
+++ script.js
@@ -1,121 +1,80 @@
-//Step 1: getComputerChoice
+//getComputerChoice
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3 + 1);
     return choice === 1 ? "rock"
         : choice === 2 ? "paper"
         : "scissors";
-}
+};
 
+//Scores
 
-//Step 2: getHumanChoice
-
-function getHumanChoice() {
-    let choice = prompt("Rock, paper, scissors - make your choice!").toLowerCase();
-    return choice === "rock" || choice === "paper" || choice === "scissors" ? choice
-    : alert( "Please input rock, paper or scissors." );
-}
-
-
-//Step 3: Scores
-
+let score = [0, 0]; //human, computer
 let humanScore = 0;
 let computerScore = 0;
 
-//Step 4: Round
-const points = document.querySelectorAll(".points div");
-console.log(points);
+const humanPoints = document.querySelectorAll("#humanScore .empty");
+const computerPoints = document.querySelectorAll("#computerScore .empty");
 
-function youLose() {
-    alert( "You lost! Score: " + humanScore + " : " + ++computerScore);
-    return computerScore;
-}
 
-function youWin() {
-    alert( "You won! Score: " + ++humanScore + " : " + computerScore);
-    return humanScore;
-}
-
-function draw() {
-    alert( "It's a draw! Score: " + humanScore + " : " + computerScore);
-}
+//Round
 
 function playRound(computerChoice, humanChoice) {
-    switch (humanChoice) {
-        case "rock": switch (computerChoice) {
-            case "paper":
-                youLose();
-                return computerScore;
-                break;
-            case "scissors":
-                youWin();
-                return humanScore;
-                break;
-            default:
-                draw()
-                break;
-        }   break;
-        case "paper": switch (computerChoice) {
-            case "scissors":
-                youLose();
-                return computerScore;
-                break;
-            case "rock":
-                youWin();
-                return humanScore;
-                break;
-            default:
-                draw()
-                break;
-        }   break;
-        case "scissors": switch (computerChoice) {
-            case "rock":
-                youLose();
-                return computerScore;
-                break;
-            case "paper":
-                youWin();
-                return humanScore;
-                break;
-            default:
-                draw()
-                break;
-        }   break;
-    }
-}
+    const choices = [computerChoice, humanChoice];
+    
+    if (        //player wins
+        (choices[0] === "rock") && (choices[1] === "paper")
+        || (choices[0] === "paper") && (choices[1] === "scissors")
+        || (choices[0] === "scissors") && (choices[1] === "rock")
+    ) {
+        ++score[0];
+        alert( "You won! Score: " + ++humanScore + " : " + computerScore);
+        humanPoints[score[0]-1].classList.add("full");
+    } else if ( //computer wins
+        (choices[1] === "rock") && (choices[0] === "paper")
+        || (choices[1] === "paper") && (choices[0] === "scissors")
+        || (choices[1] === "scissors") && (choices[0] === "rock")
+    ) {
+        ++score[1];
+        computerPoints[score[0]-1].classList.add("full");
+        alert( "You lost! Score: " + humanScore + " : " + ++computerScore);
+    } else {    //draw
+        alert( "It's a draw! Score: " + humanScore + " : " + computerScore);
+    };
+    return score;
+};
 
+// Game
 
 function playGame() {
-    /*for (let round = 1; round < 6; round++) {
-        console.log("Round " + round + " of 5")
-        console.log(playRound(getComputerChoice(), getHumanChoice()))
-    }*/
     const message = document.querySelector("#end");
     const btns = document.querySelector(".button-wrapper");
 
+    //while (score[0] < 5 && score[1] < 5) {
     btns.addEventListener('click', (event) => {
         let target = event.target;
         switch (target.id) {
             case 'rockBtn':
                 alert("You chose rock!");
-                alert(playRound(getComputerChoice(), "rock"));
+                score = playRound(getComputerChoice(), "rock");
                 break;
             case 'paperBtn':
                 alert("You chose paper!");
-                alert(playRound(getComputerChoice(), "paper"));
+                score = playRound(getComputerChoice(), "paper");
                 break;
             case 'sciBtn':
                 alert("You chose scissors!");
-                alert(playRound(getComputerChoice(), "paper"));
+                score = playRound(getComputerChoice(), "scissors");
                 break;
-        }
+        };
+
+        if (score[0] === 5) {
+            message.textContent = "Congrats, you won the game!";
+        };
+        if (score[1] === 5) {
+            message.textContent = "You lost the game...";
+        };
     });
+};
 
-    if (humanScore > computerScore) {
-        message.textContent = "Congrats, you won the game!";
-    } else if (humanScore < computerScore) {
-        message.textContent = "You lost the game...";
-    };
-}
-
-playGame()
+playGame();
