fc25a01ef3ec921e4dfe0b583d37a8feef2c66e4
Luisa-jpg <luisa.metzen@arcor.de>
2024-05-20 13:35:13 +0200

    Add replay button, reset and endGame functions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice() {
         : "scissors";
 };
 
-//Scores
+// Scores
 
 let score = [0, 0]; //human, computer
 let humanScore = 0;
@@ -16,31 +16,45 @@ let computerScore = 0;
 const humanPoints = document.querySelectorAll("#humanScore .empty");
 const computerPoints = document.querySelectorAll("#computerScore .empty");
 
+// Status 
+const statusDiv = document.querySelector(".status");
+const statusP = document.querySelector(".status p");
+const ComMessage = "The computer chose ";
+const wMessage = "You won. ";
+const lMessage = "You lost. ";
+const drawMessage = "It's a draw. ";
 
-//Round
+// Replay Button
+const replayBtn = document.createElement("button");
+const pEmpty = document.createElement("p");
+const icon = document.createElement("i");
+const pReplay = document.createElement("p");
+
+// Round
 
 function playRound(computerChoice, humanChoice) {
     const choices = [computerChoice, humanChoice];
-    
+
     if (        //player wins
         (choices[0] === "rock") && (choices[1] === "paper")
         || (choices[0] === "paper") && (choices[1] === "scissors")
         || (choices[0] === "scissors") && (choices[1] === "rock")
     ) {
         ++score[0];
-        alert( "You won! Score: " + ++humanScore + " : " + computerScore);
         humanPoints[score[0]-1].classList.add("full");
+        statusP.textContent = wMessage + ComMessage + computerChoice + "!";
     } else if ( //computer wins
         (choices[1] === "rock") && (choices[0] === "paper")
         || (choices[1] === "paper") && (choices[0] === "scissors")
         || (choices[1] === "scissors") && (choices[0] === "rock")
     ) {
         ++score[1];
-        computerPoints[score[0]-1].classList.add("full");
-        alert( "You lost! Score: " + humanScore + " : " + ++computerScore);
+        computerPoints[score[1]-1].classList.add("full");
+        statusP.textContent = lMessage + ComMessage + computerChoice + "!";
     } else {    //draw
-        alert( "It's a draw! Score: " + humanScore + " : " + computerScore);
+        statusP.textContent = drawMessage + ComMessage + computerChoice + "!";
     };
+    
     return score;
 };
 
@@ -50,31 +64,58 @@ function playGame() {
     const message = document.querySelector("#end");
     const btns = document.querySelector(".button-wrapper");
 
-    //while (score[0] < 5 && score[1] < 5) {
-    btns.addEventListener('click', (event) => {
-        let target = event.target;
-        switch (target.id) {
+    btns.addEventListener('click', (e) => { ; //mit closest() lösen?
+        switch (e.target.id) {
             case 'rockBtn':
-                alert("You chose rock!");
                 score = playRound(getComputerChoice(), "rock");
                 break;
             case 'paperBtn':
-                alert("You chose paper!");
                 score = playRound(getComputerChoice(), "paper");
                 break;
             case 'sciBtn':
-                alert("You chose scissors!");
                 score = playRound(getComputerChoice(), "scissors");
                 break;
         };
 
         if (score[0] === 5) {
             message.textContent = "Congrats, you won the game!";
+            endGame();
         };
         if (score[1] === 5) {
             message.textContent = "You lost the game...";
+            endGame();
         };
-    });
+    }, false);
+};
+
+function endGame() {
+    document.body.insertBefore(replayBtn, statusDiv);
+    replayBtn.appendChild(pEmpty);
+    replayBtn.appendChild(icon);
+    replayBtn.appendChild(pReplay);
+
+    replayBtn.id = "#replay";
+    icon.classList.add("fa-solid", "fa-rotate", "fa-3x", "fa-spin");
+    pReplay.textContent = "replay";
+
+    replayBtn.addEventListener("click", reset);
+};
+
+function reset() {
+    statusP.textContent = "Make a choice!";
+    for (const el of humanPoints) {
+        el.classList.remove("full");
+        el.classList.add("empty");
+    };
+    for (const el of computerPoints) {
+        el.classList.remove("full");
+        el.classList.add("empty");
+    };
+
+    score = [0, 0];
+
+    document.body.removeChild(replayBtn);
 };
 
 playGame();
+
